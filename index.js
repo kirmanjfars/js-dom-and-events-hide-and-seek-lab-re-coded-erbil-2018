9b7179581c7f527e54284b4435f18327fc468499
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 14:56:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+ function getFirstSelector(selector){
+   
+   let selected = document.querySelector(selector);
+   
+   return selected;
+ } 
+ 
+ function nestedTarget(){
+    return document.querySelector('#nested .target')
+ }
+ 
+ function increaseRankBy(n){
+    const rankedLists = document.querySelectorAll('.ranked-list');
+    
+    
+ }
